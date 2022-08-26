7cc5fa193182eb7b47548b20f3b8518d61e84f1e
Vitor Serigati de Oliveira Basso <vitor_serigati@hotmail.com>
2022-08-26 04:00:02 -0300

    Testing lvim

--- Aulas/Conditional_operators/ternary_operator.rb
+++ Aulas/Conditional_operators/ternary_operator.rb
@@ -1,5 +1,10 @@
-puts "Type your age: "
+# frozen_string_literal: true
+
+puts 'Type your age: '
 
 age = gets.to_i
-response = age < 18 ? "You still have your entire life ahead of you." : "You're all grown up."
+response = age < 18 ? 'You still have your entire life ahead of you.' : "You're all grown up."
 puts response #=> "You're all grown up."
+
+
+
