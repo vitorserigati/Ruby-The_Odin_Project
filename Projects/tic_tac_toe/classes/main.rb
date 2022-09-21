f9204ecc3658570f65ebb5d8ab626738dd122e78
Vitor Serigati de Oliveira Basso <79308595+vitorserigati@users.noreply.github.com>
2022-09-21 06:33:43 -0300

    Project tic-toc-toe (jogo da velha)

---
+++ Projects/tic_tac_toe/classes/main.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+table = Array.new(9, '1')
+
+puts "#{table[0]}|#{table[1]}|#{table[2]}\n"\
+     "-----------------------------------\n"\
+     "#{table[3]}|#{table[4]}|#{table[5]}\n"\
+     "-----------------------------------\n"\
+     "#{table[6]}|#{table[7]}|#{table[8]}#{table[9]}  "
